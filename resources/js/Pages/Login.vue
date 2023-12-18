3869e710d2b9f5bd29b1639cfde737e9721f9071
Jay <jay@entigy.co.uk@>
2023-12-18 17:31:13 +0000

    chore: migrate login page to inertia form

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -3,21 +3,22 @@
         <v-container>
             <div class="d-flex justify-center align-center vh-100">
                 <v-card variant="elevated" width="400">
-                    <v-toolbar color="primary" dark flat>
+                    <v-toolbar color="accent" dark flat>
                         <v-toolbar-title>Login</v-toolbar-title>
                     </v-toolbar>
                     <v-card-text>
-                        <v-form @submit.prevent="submit">
+                        <v-form @submit.prevent="form.post(route('auth.authenticate'))">
                             <v-text-field
                                 v-model="form.email"
-                                :error="errors.email"
+                                :error="form.errors.email"
+                                :error-messages="form.errors.email"
                                 label="Email"
                                 name="email"
                                 type="email"
+                                class="mb-2"
                             ></v-text-field>
                             <v-text-field
                                 v-model="form.password"
-                                :error="errors.password"
                                 label="Password"
                                 name="password"
                                 type="password"
@@ -32,11 +33,6 @@
                                 <v-btn color="primary" type="submit">Login</v-btn>
                             </v-card-actions>
                         </v-form>
-                        <template v-if="errors.email">
-                            <v-alert type="error" dismissible>
-                                {{ errors.email }}
-                            </v-alert>
-                        </template>
                     </v-card-text>
                 </v-card>
             </div>
@@ -44,23 +40,11 @@
     </v-app>
 </template>
 <script setup>
-import { reactive } from "vue";
-import { router } from "@inertiajs/vue3";
+import { useForm } from "@inertiajs/vue3";
 
-defineProps({
-    errors: {
-        type: Object,
-        default: () => ({}),
-    },
-});
-
-const form = reactive({
+const form = useForm({
     email: "",
     password: "",
     remember: false,
 });
-
-const submit = () => {
-    router.post(route('auth.authenticate'), form);
-};
 </script>
